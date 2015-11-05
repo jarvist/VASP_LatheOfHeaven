c5a6fc46d9ea99099ca6dd897bf7da9c46e733ce
Jarvist Moore Frost <jarvist@gmail.com>
2015-11-05 16:50:33 +0000

    Document ISIF, IBRION; attempt some sensible recipes.

--- VASP_LatheOfHeaven.sh
+++ VASP_LatheOfHeaven.sh
@@ -7,6 +7,16 @@
 
 VASP="mpirun -np 24 /opt/vasp5.3.5-gamma"
 
+# IBRION = 1 ; RMM-DIIS
+# IBRION = 2 ; conjugate gradients. STEP=1, forces, STEP=2, Trial, STEP=3, correction, and so on. 
+# IBRION = 3 ; Damped MD. Supply SMASS and POTIM
+
+# ISIF = 2 ; Move atoms only
+# ISIF = 3 ; Move atoms, change unit cell shape and volume
+# ISIF = 5 ; Cell shape only
+# ISIF = 6 ; Cell shape and volume
+# ISIF = 7 ; Cell volume only
+
 recipe[1]="IBRION = 1
 POTIM = 0.15
 ISIF = 7 
@@ -17,17 +27,22 @@ POTIM = 0.15
 ISIF = 5 
 NSW = 3"
 
-recipe[3]="IBRION = 1
+recipe[3]="IBRION = 2
 POTIM = 0.15
 ISIF = 2
-NSW = 3"
+NSW = 7"
 
 recipe[4]="IBRION = 1
 POTIM = 0.15
-ISIF = 2 
-NSW = 3"
+ISIF = 6 
+NSW = 5"
 
-recipes=4
+recipe[5]="IBRION = 2
+POTIM = 0.15
+ISIF = 3 
+NSW = 9"
+
+recipes=5
 
 for id in ` seq ${recipes} `
 do
